7a39c7b0afa6e5126bc96bc55cb1158f681b3a8b
Tqkoyaki <np768@scarletmail.rutgers.edu>
2022-12-06 11:34:33 -0500

    Day Five Complete

---
+++ dayFive/partOne.py
@@ -0,0 +1,49 @@
+inp = open('dayFive/input.txt', 'r')
+
+# Data structure for crates
+crates = [[], [], [], [], [], [], [], [], []]
+
+# Loops through crate schema
+line = inp.readline()
+while True:
+    # Ignores after crate schema is complete
+    if line.replace(" ", "").strip().isnumeric():
+        break
+    
+    # Splits the line into a list of characters
+    line = [*line[1::4]]
+    
+    # Appends each section appropriately
+    for i in range(9):
+        if line[i] != ' ':
+            crates[i].append(line[i])
+    
+    # Updates line
+    line = inp.readline()
+
+# Reverses each list for stacks
+for i in range(len(crates)):
+    crates[i] = crates[i][::-1]
+    
+# Skips the empty line
+line = inp.readline()
+
+# Loops through the moves
+for line in inp:
+    # Gets only the numbers
+    line = line.split()[1::2]
+    
+    # Moves each crate to new stack and removes from old stack
+    for i in range(int(line[0])):
+        crates[int(line[2]) - 1].append(crates[int(line[1]) - 1].pop())
+
+# Appends the top of each stack to a string to get the result
+res = ''
+for i in range(len(crates)):
+    res += crates[i].pop()
+
+print(res)
+    
+
+
+    
---
+++ dayFive/partTwo.py
@@ -0,0 +1,51 @@
+inp = open('dayFive/input.txt', 'r')
+
+# Data structure for crates
+crates = [[], [], [], [], [], [], [], [], []]
+
+# Loops through crate schema
+line = inp.readline()
+while True:
+    # Ignores after crate schema is complete
+    if line.replace(" ", "").strip().isnumeric():
+        break
+    
+    # Splits the line into a list of characters
+    line = [*line[1::4]]
+    
+    # Appends each section appropriately
+    for i in range(9):
+        if line[i] != ' ':
+            crates[i].append(line[i])
+    
+    # Updates line
+    line = inp.readline()
+
+# Reverses each list for stacks
+for i in range(len(crates)):
+    crates[i] = crates[i][::-1]
+    
+# Skips the empty line
+line = inp.readline()
+
+# Loops through the moves
+for line in inp:
+    # Gets only the numbers
+    line = line.split()[1::2]
+    
+    # Moves the crates preserving the order
+    crates[int(line[2]) - 1] += crates[int(line[1]) - 1][-int(line[0]):]
+    
+    # Removes the crates from the old stack
+    crates[int(line[1]) - 1] = crates[int(line[1]) - 1][:-int(line[0])]
+
+# Appends the top of each stack to a string to get the result
+res = ''
+for i in range(len(crates)):
+    res += crates[i].pop()
+
+print(res)
+    
+
+
+    
